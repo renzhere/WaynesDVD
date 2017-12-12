97cc99a348f5daadd2c520f30e8360d2dd5968f7
Reenu Mohandas <ren.akshaya@gmail.com>
2017-12-12 20:37:18 +0000

    source code

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoviesDVD.Startup))]
+namespace MoviesDVD
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
